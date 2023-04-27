084e4193751ca41ff3b6fd4391b774bff3707bed
Charan N <charan2206.naren@gmail.com>
2023-04-27 18:38:12 +0530

    Revise
    Fibonacci

---
+++ ex6.java
@@ -0,0 +1,5 @@
+public class Main { public static void main(String[ ] args) { int count = 7, num1 = 0, num2 = 1; 
+System.out.print("Fibonacci Series of "+count+" numbers : "); int i=1; do 
+ {   System.out.print(num1+"  "); int sumOfPrevTwo = num1 + num2; num1 = num2; num2 = sumOfPrevTwo; 
+i++;  } 
+while(i<=count);   } } 
